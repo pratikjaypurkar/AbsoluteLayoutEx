673930685ba9a484adb21e5b094e659c572b8a7a
pratik <pratikjaypurkar@gmail.com>
2022-04-22 10:33:41 +0530

    Initial commit

---
+++ app/src/main/java/com/hackteachz/pr_05_absolutelayout/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hackteachz.pr_05_absolutelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
